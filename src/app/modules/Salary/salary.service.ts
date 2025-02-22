5d2b6a570249601fbf5f229e218175b9a8f4ddad
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2025-02-22 12:55:15 +0600

    ADD: implement CRUD operations for salary records in salary service

--- src/app/modules/Salary/salary.service.ts
+++ src/app/modules/Salary/salary.service.ts
@@ -2,11 +2,13 @@ import QueryBuilder from '../../builder/QueryBuilder';
 import { ISalary } from './salary.interface';
 import { Salary } from './salary.model';
 
+// Creating a salary
 const createSalaryIntoDB = async (payload: ISalary) => {
   const salary = await Salary.create(payload);
   return salary;
 };
 
+// Get all salary from the DB
 const getAllSalaryFromDB = async (query: Record<string, unknown>) => {
   const salary = new QueryBuilder(Salary.find(), query).sort().paginate();
   const meta = await salary.countTotal();
@@ -19,7 +21,30 @@ const getAllSalaryFromDB = async (query: Record<string, unknown>) => {
   };
 };
 
+// Get single salary
+const getSingleSalaryFromDB = async (salaryId: string) => {
+  const salary = await Salary.findById(salaryId);
+  return salary;
+};
+
+// Update Salary
+const updateSalaryIntoDB = async (salaryId: string, payload: ISalary) => {
+  const salary = await Salary.findByIdAndUpdate(salaryId, payload, {
+    new: true,
+  });
+  return salary;
+};
+
+// Delete Salary
+const deleteSalaryFromDB = async (salaryId: string) => {
+  const salary = await Salary.findByIdAndDelete(salaryId);
+  return salary;
+};
+
 export const SalaryServices = {
   createSalaryIntoDB,
   getAllSalaryFromDB,
+  getSingleSalaryFromDB,
+  updateSalaryIntoDB,
+  deleteSalaryFromDB,
 };
