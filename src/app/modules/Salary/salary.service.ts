b59b38feca802f14dd31544527dce87a256e1469
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2025-02-22 17:29:50 +0600

    ADD: implement salary creation with transaction handling

--- src/app/modules/Salary/salary.service.ts
+++ src/app/modules/Salary/salary.service.ts
@@ -1,11 +1,45 @@
 import QueryBuilder from '../../builder/QueryBuilder';
+import { TransactionType } from '../Transaction/transaction.constant';
+import { Transaction } from '../Transaction/transaction.model';
 import { ISalary } from './salary.interface';
 import { Salary } from './salary.model';
 
 // Creating a salary
+/**
+ *
+ * @param payload
+ * @returns
+ *
+ * This function will use session to handle the changes in the Salary collection as well as the Transaction Collection
+ */
 const createSalaryIntoDB = async (payload: ISalary) => {
-  const salary = await Salary.create(payload);
-  return salary;
+  const session = await Salary.startSession();
+  session.startTransaction();
+
+  try {
+    const salary = await Salary.create([payload], { session });
+    const transactionPayload = {
+      userId: payload.userId,
+      amount: payload.amount,
+      transactionType: TransactionType.DEPOSIT,
+      transactionSource: 'SALARY',
+      transactionDate: payload.paymentDate,
+      transactionId: payload.transactionId,
+      note: payload.note,
+    };
+    await Transaction.create([transactionPayload], {
+      session,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return salary[0];
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
 };
 
 // Get all salary from the DB
